f4438820ed8a1e68247138dfe1fd940afe0a1674
yeshengwu510168 <evan@macmini.com>
2022-12-10 17:28:40 +0800

    添加 groovy 入门demo 闭包closure.

--- Student.groovy
+++ Student.groovy
@@ -2,7 +2,7 @@
 
 class Student implements GroovyInterceptable {
     def name
-
+    // todo 加了 invokeMethod 这个方法不执行了，为什么？
     def hello() {
         println('Hello ${name}') // ${name} 这种用法和 python 一样
     }
@@ -10,7 +10,9 @@ class Student implements GroovyInterceptable {
     @Override
     Object invokeMethod(String name, Object args) {
         System.out.println "invokeMethod : $name"
+    }
 
-//        return super.invokeMethod(name, args)
+    Object methodMissing(String name, Object args) {
+        System.out.println "methodMissing : $name"
     }
 }
--- TestGroovy.groovy
+++ TestGroovy.groovy
@@ -1,5 +1,10 @@
 println 'Hello'
 
+// 重要资料：
+// https://www.cnblogs.com/wxdlut/p/15589685.html#top  Groovy入门
+// https://juejin.cn/post/6954350461818421278#heading-1 5 分钟的 Java 转 Groovy 教程
+// https://juejin.cn/post/7092367604211253256#heading-8 gradle 进阶
+
 // There is a synthetic class 'TestGroovy' generated for script code
 //class TestGroovy {
 //
@@ -7,20 +12,44 @@ println 'Hello'
 // 根据out生成的目录得知， TestGroovy.groovy 编译器会生成一个同名的类：  public class TestGroovy extends Script {}
 // 如果这个时候再 定义一个同名的 class TestGroovy 就会抛出上面的编译异常导致无法编译。
 
-// 重要资料：
-// https://www.cnblogs.com/wxdlut/p/15589685.html#top  Groovy入门
-// https://juejin.cn/post/6954350461818421278#heading-1 5 分钟的 Java 转 Groovy 教程
-// https://juejin.cn/post/7092367604211253256#heading-8 gradle 进阶
+// 这样就可以： Test 类跟文件名不同 这个是 Closure类中的sample
+class Test {
+    def x = 30
+    def y = 40
 
+    def run() {
+        def data = [ x: 10, y: 20 ]
+        def cl = { y = x + y }
+        cl.delegate = data
+        cl.resolveStrategy = Closure.DELEGATE_FIRST
+        cl()
+        assert x == 30
+        assert y == 40
+        assert data == [x:10, y:30]
+    }
+}
+// 做个测试调一下看看
+new Test().run()
+// Closure.OWNER_FIRST（默认）： 优先在 owner 对象中寻找，再去 delegate 对象中寻找；
+//Closure.DELEGATE_FIRST： 优先在 delegate 对象中寻找，再去 owner 对象中寻找；
+//Closure.OWNER_ONLY： 只在 owner 对象中寻找；
+//Closure.DELEGATE_ONLY： 只在 delegate 对象中寻找；
+//Closure.TO_SELF： 只在闭包本身寻找；
 
+name = 'Wangfang'
+age = '18'
 
+// 占位符拼接的用法
+println("my name is ${name},my age is ${age}.")
 
-println("ls -l")
 
 def x = 10
 def y = 25
 
-print 'Sum of x + y = ' + (x + y)
+println 'Sum of x + y = ' + (x + y)
+
+println 'methodName return value = ' +methodName()
+println 'methodName2 return value = ' +methodName2()
 
 
 // todo 搞一个 入门的 groovy 工程来完成1.0版本的 groovy 语言学习。
@@ -28,13 +57,13 @@ print 'Sum of x + y = ' + (x + y)
 // Gradle 系列（1）为什么说 Gradle 是 Android 进阶绕不去的坎
 // https://juejin.cn/post/7092367604211253256#heading-8
 
-// 函数定义
+// 函数定义 默认最后一行语句为返回值，不一定要 return keyword.
 def methodName() {
-
+    "In methodName def lastCode no return keyword"
 }
 
 String methodName2() {
-
+    "In methodName2:String lastCode no return keyword"
 }
 
 // 省略参数类型
@@ -70,3 +99,26 @@ def methodName5() {
 //    }
 //}
 
+
+// 带隐式参数 it
+def greeting = { "Hello, $it!" }
+assert greeting('Patrick') == 'Hello, Patrick!'
+
+// 不带隐式参数 it
+//def magicNumber = { -> 42 }
+// error 不允许传递参数： No signature of method: TestGroovy$_run_closure2.call() is applicable for argument types: (Integer) values: [11]
+def magicNumber = { it -> it+42 }
+var magicNumberResult = magicNumber(11)
+printf("magicNumberResult: $magicNumberResult") // 53
+
+// 闭包参数简化： 函数的最后一个参数是闭包类型的化，在调用时可以简化，省略圆括号：
+def methodName(String param1, Closure closure) {
+    // Method Code
+}
+
+// 调用：
+methodName("Hello") {
+    // Closure Code
+}
+//----evan: 这个跟 kotlin 他妈的一样的套路。
+// ------
